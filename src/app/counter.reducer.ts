735c5b74f1b4a1308d9f049b8f314390483cfafd
hassanKhademi <hassankhademi67@gmail.com>
2021-06-08 16:00:50 +0430

    1 file modified

--- src/app/counter.reducer.ts
+++ src/app/counter.reducer.ts
@@ -1,21 +1,18 @@
-import {createReducer,on} from "@ngrx/store";
-import * as actions from "./counter.action";
+import { createReducer, on } from '@ngrx/store';
+import * as actions from './counter.action';
 const initCounter = 10;
 
 const _counterReducer = createReducer(
   initCounter,
- on(actions.increase,(state)=>state+1),
- on(actions.decrease,(state)=>state-1),
- on(actions.reset,(state)=> 0),
- on(actions.setCustom,
- (state,{val})=>  {
+  on(actions.increase, state => state + 1),
+  on(actions.decrease, state => state - 1),
+  on(actions.reset, state => 0),
+  on(actions.setCustom, (state, { val }) => {
+    console.log('newVlue', val, state);
+    return val;
+  })
+);
 
-    console.log("newVlue",val,state);
-    return val
-
- })
-)
-
-export function counterReducer(state,action){
-   return _counterReducer(state,action);
-} 
+export function counterReducer(state, action) {
+  return _counterReducer(state, action);
+}
